8296a442356773ff742f070191abf046698c410c
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-04-15 14:22:16 +0200

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '../views/Home'
-import About from '../views/About'
-import Contact from '../views/Contact'
+import Home from '@/views/Home'
+import About from '@/views/About'
+import Contact from '@/views/Contact'
 
 Vue.use(Router)
 
