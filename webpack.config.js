907cf171cfc73ac065f2be5c3850f94f0132e3ef
adr1enbe4udou1n <adrien.beaudouin@outlook.com>
2018-04-15 13:05:16 +0200

    runtime only

--- webpack.config.js
+++ webpack.config.js
@@ -119,7 +119,6 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
       '@': path.join(__dirname, 'src')
     }
   },
