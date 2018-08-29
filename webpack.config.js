728bb01e3fcadd39945893a5243a2b78c2c3dbfc
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2018-08-29 17:06:46 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -82,10 +82,6 @@ module.exports = {
         }
       },
       {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
